e6c7ec779356955c99ee063f3bc3e1a7cd7739f3
Slava <rtootik@gmail.com>
2017-03-19 18:42:36 +0300

    Initial Commit

---
+++ 10-HomeWork-BitsTests/_0_HomeWork_BitsTests.m
@@ -0,0 +1,39 @@
+//
+//  _0_HomeWork_BitsTests.m
+//  10-HomeWork-BitsTests
+//
+//  Created by Slava on 19.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0_HomeWork_BitsTests : XCTestCase
+
+@end
+
+@implementation _0_HomeWork_BitsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
